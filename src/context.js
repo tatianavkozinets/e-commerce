ce78337f9dac9480c903aa500e61c2982376d954
Tatiana <tatianavkozinets@gmail.com>
2020-02-17 17:18:50 +0200

    some changes

--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ const ProductContext = React.createContext();
 const ProductProvider = props => {
   const [storeProducts, setStoreProducts] = useState([]);
   const [detailProduct, setDetailProduct] = useState(b);
-  const [cart, setCart] = useState([detailProduct]);
+  const [cart, setCart] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [modalProduct, setModalProduct] = useState(detailProduct);
   const [cartSubTotal, setCartSubTotal] = useState(0);
@@ -20,20 +20,20 @@ const ProductProvider = props => {
   useEffect(() => {
     console.log("useEffect");
 
-      const addTotals = () => {
-          let subTotal = 0;
-          cart.map(i => (subTotal += i.total));
-          const tempTax = subTotal * 0.1;
-          const tax = parseFloat(tempTax.toFixed(2));
-          const total = subTotal + tax;
-          setCartSubTotal(subTotal);
-          setCartTax(tax);
-          setCartTotal(total);
-      };
+    const addTotals = () => {
+      let subTotal = 0;
+      cart.map(i => (subTotal += i.total));
+      const tempTax = subTotal * 0.1;
+      const tax = parseFloat(tempTax.toFixed(2));
+      const total = subTotal + tax;
+      setCartSubTotal(subTotal);
+      setCartTax(tax);
+      setCartTotal(total);
+    };
 
     addTotals();
     // });
-   }, [cart]);
+  }, [cart]);
 
   const setProduct = () => {
     let tempProduct = [];
@@ -101,9 +101,7 @@ const ProductProvider = props => {
     const index = tempCart.indexOf(selectedProduct);
     const product = tempCart[index];
 
-    if (product.count === 1) {
-      return;
-    } else {
+    if (product.count !== 1) {
       product.count -= 1;
       product.total = product.price * product.count;
       setCart([...tempCart]);
@@ -130,8 +128,6 @@ const ProductProvider = props => {
     setProduct();
   };
 
-
-
   return (
     <ProductContext.Provider
       value={{
@@ -147,9 +143,6 @@ const ProductProvider = props => {
         addToCart,
         openModal,
         closeModal,
-        increment,
-        decrement,
-        removeItem,
         clearCart
       }}
     >
